3f1a03e733b5750c20db2a205975acbc532399cb
Bundele-Durwangsingh <bundeledurwang@gmail.com>
2024-09-24 13:34:15 +0530

    Update: Day2 Best Time to Buy and Sell Stock and 2sum optimal

---
+++ src/BestTimetoBuyandSellStock.java
@@ -0,0 +1,22 @@
+public class BestTimetoBuyandSellStock {
+    public static void main(String[] args) {
+        int[] prices={7,1,5,3,6,4};
+        System.out.println(maxProfit(prices));
+    }
+    public static int maxProfit(int[] prices) {
+        if (prices.length<2 || prices==null){
+            return 0;
+        }
+        int minvalue=Integer. MAX_VALUE;
+        int maxprofit=0;
+
+        for (int i=0;i<prices.length;i++){
+            if (prices[i]<minvalue){
+                minvalue=prices[i];
+            }else if(prices[i]-minvalue>maxprofit){
+                maxprofit=prices[i]-minvalue;
+            }
+        }
+        return maxprofit;
+    }
+}
--- src/TwoSum.java
+++ src/TwoSum.java
@@ -1,17 +1,33 @@
+import java.util.HashMap;
+import java.util.Map;
 public class TwoSum {
     public static void main(String[] args) {
-        int[] nums= {2,7,11,15};
-        int target =9;
-        System.out.println(twoSum(nums, target));
+        int[] nums = { 2, 7, 11, 15 };
+        int target = 9;
+        System.out.println(twoSum(nums, target));//For burte force
+        System.out.println(twoSumoptimal(nums, target));//For optimal
     }
+
     public static int[] twoSum(int[] nums, int target) {
-        for (int i = 0;i<nums.length;i++){
-            for(int j =i+1;j<nums.length;j++){
-                if (nums[i]+nums[j]==target){
-                    System.out.println(i+","+j);
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    System.out.println(i + "," + j);
                 }
             }
         }
         return nums;
+    }
+
+    public static int[] twoSumoptimal(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i =0 ;i<nums.length;i++){
+            int x= target -nums[i];
+            if(map.containsKey(x)){
+                return new int []{map.get(x),i};
+            }
+            map.put(nums[i],i);
+        }
+        return new int[]{};
     }
 }
